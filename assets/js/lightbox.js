c8fa40220dff30036b927b2f100da09fbea5806d
jartuso <138617496+FlatlineMcCoy@users.noreply.github.com>
2023-07-20 08:51:56 -0400

    reduce lightbox.js durations

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -43,16 +43,16 @@
   Lightbox.defaults = {
     albumLabel: 'Image %1 of %2',
     alwaysShowNavOnTouchDevices: false,
-    fadeDuration: 600,
+    fadeDuration: 300,
     fitImagesInViewport: true,
-    imageFadeDuration: 600,
+    imageFadeDuration: 300,
     // maxWidth: 800,
     // maxHeight: 600,
     positionFromTop: 50,
-    resizeDuration: 700,
+    resizeDuration: 300,
     showImageNumberLabel: true,
     wrapAround: false,
-    disableScrolling: false,
+    disableScrolling: true,
     /*
     Sanitize Title
     If the caption data is trusted, for example you are hardcoding it in, then leave this to false.
